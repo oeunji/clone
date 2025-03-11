9789ead48b4c6491788d80d19230fdd51e041247
eunji <oieunxe@gmail.com>
2025-03-11 21:37:51 +0900

    xcode 추가

---
+++ clone_test/clone_testTests/clone_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  clone_testTests.swift
+//  clone_testTests
+//
+//  Created by 이은지 on 3/11/25.
+//
+
+import Testing
+@testable import clone_test
+
+struct clone_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
